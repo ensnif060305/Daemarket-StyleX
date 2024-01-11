e1f6b057ae10d15a32bad4eeab94f467eda1a0f1
ensnif060305 <jmsun0305@gmail.com>
2024-01-11 17:14:17 +0900

    ✨ password router 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "@/pages/LoginPage"
 import SignupPage from "@/pages/SignupPage"
+import PasswordPage from "./pages/PasswordPage"
 
 export const Router = () => {
   return (
@@ -8,6 +9,7 @@ export const Router = () => {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/password" element={<PasswordPage />} />
       </Routes>
     </BrowserRouter>
   )
