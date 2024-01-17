6d15fe06ba34350a70090f0ff5d5de7af1d5aaf5
ensnif060305 <jmsun0305@gmail.com>
2024-01-17 10:18:42 +0900

    fix: layout 페이지별 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "@/pages/LoginPage"
 import SignupPage from "@/pages/SignupPage"
 import PasswordPage from "./pages/PasswordPage"
+import MainPage from "./pages/used/MainPage"
+import { HeaderLayout } from "./pages/HeaderLayout"
+import { FooterLayout } from "./pages/FooterLayout"
+import ChatPage from "@/pages/chat/ChatPage"
 
 export const Router = () => {
   return (
@@ -10,6 +14,12 @@ export const Router = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/password" element={<PasswordPage />} />
+        <Route element={<HeaderLayout />}>
+          <Route element={<FooterLayout />}>
+            <Route path="/used/main" element={<MainPage />} />
+          </Route>
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
