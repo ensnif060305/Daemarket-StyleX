01183948f0c326058bcc3bed80695dddc4087994
ensnif060305 <jmsun0305@gmail.com>
2024-01-19 15:45:15 +0900

    fix: style 수정

--- src/pages/HeaderLayout.tsx
+++ src/pages/HeaderLayout.tsx
@@ -1,13 +1,26 @@
 import { Header } from "@/components/Header"
 import { Outlet } from "react-router-dom"
+import * as stylex from "@stylexjs/stylex"
 
 export const HeaderLayout = () => {
   return (
-    <div>
-      <div>
+    <div {...stylex.props(style.container)}>
+      <div {...stylex.props(style.wrapper)}>
         <Header />
-        <Outlet/>
+        <Outlet />
       </div>
     </div>
   )
 }
+
+const style = stylex.create({
+  container: {
+    width: "100vw",
+    height: "100vh",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+})
