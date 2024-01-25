91087175b12489687f3d66d02be37302c8ddae34
ensnif060305 <jmsun0305@gmail.com>
2024-01-25 09:43:11 +0900

    fix: all page router 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,7 +3,9 @@ import LoginPage from "@/pages/LoginPage"
 import SignupPage from "@/pages/SignupPage"
 import PasswordPage from "./pages/PasswordPage"
 import UsedMainPage from "./pages/used/MainPage"
+import UsedAllPage from "./pages/used/AllPage"
 import GroupMainPage from "./pages/group/MainPage"
+import GroupAllPage from "./pages/group/AllPage"
 import { HeaderLayout } from "./pages/HeaderLayout"
 import { FooterLayout } from "./pages/FooterLayout"
 import ChatPage from "@/pages/chat/ChatPage"
@@ -18,7 +20,9 @@ export const Router = () => {
         <Route element={<HeaderLayout />}>
           <Route element={<FooterLayout />}>
             <Route path="/used/main" element={<UsedMainPage />} />
+            <Route path="/used/all" element={<UsedAllPage />} />
             <Route path="/group/main" element={<GroupMainPage />} />
+            <Route path="/group/all" element={<GroupAllPage />} />
           </Route>
           <Route path="/chat" element={<ChatPage />} />
         </Route>
