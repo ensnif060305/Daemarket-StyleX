45559599b513e847488e412243b72a545582ba9f
ensnif060305 <jmsun0305@gmail.com>
2024-01-26 15:22:34 +0900

    fix: write page router 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "@/pages/LoginPage"
 import SignupPage from "@/pages/SignupPage"
-import PasswordPage from "./pages/PasswordPage"
-import UsedMainPage from "./pages/used/MainPage"
-import UsedAllPage from "./pages/used/AllPage"
-import GroupMainPage from "./pages/group/MainPage"
-import GroupAllPage from "./pages/group/AllPage"
-import { HeaderLayout } from "./pages/HeaderLayout"
-import { FooterLayout } from "./pages/FooterLayout"
-import ChatPage from "@/pages/chat/ChatPage"
+import PasswordPage from "@/pages/PasswordPage"
+import UsedMainPage from "@/pages/used/MainPage"
+import UsedAllPage from "@/pages/used/AllPage"
+import GroupMainPage from "@/pages/group/MainPage"
+import GroupAllPage from "@/pages/group/AllPage"
+import WritePage from "@/pages/WritePage"
+import { HeaderLayout } from "@/pages/HeaderLayout"
+import { FooterLayout } from "@/pages/FooterLayout"
 
 export const Router = () => {
   return (
@@ -18,13 +18,14 @@ export const Router = () => {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/password" element={<PasswordPage />} />
         <Route element={<HeaderLayout />}>
+          <Route path="/write" element={<WritePage />} />
+          <Route path="/group/main" element={<GroupMainPage />} />
           <Route element={<FooterLayout />}>
             <Route path="/used/main" element={<UsedMainPage />} />
             <Route path="/used/all" element={<UsedAllPage />} />
             <Route path="/group/main" element={<GroupMainPage />} />
             <Route path="/group/all" element={<GroupAllPage />} />
           </Route>
-          <Route path="/chat" element={<ChatPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
